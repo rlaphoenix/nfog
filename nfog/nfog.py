f3249c212a16425a24b342297bd9ccbf6147ed5f
rlaphoenix <pragma.exe@gmail.com>
2022-01-31 04:42:54 +0000

    Add version command to list project version

--- nfog/nfog.py
+++ nfog/nfog.py
@@ -1,5 +1,6 @@
 import click
 
+from nfog import __version__
 from nfog.constants import GROUP_SETTINGS
 
 
@@ -11,3 +12,13 @@ def cli() -> None:
     Scriptable Database-Driven NFO Generator for Movies and TV.
     https://github.com/rlaphoenix/nfog
     """
+
+
+@cli.command()
+def version() -> None:
+    """Shows the version of the project."""
+    print(
+        f"nfog {__version__}\n"
+        f"Scriptable Database-Driven NFO Generator for Movies and TV.\n"
+        "https://github.com/rlaphoenix/nfog"
+    )
