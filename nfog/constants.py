b9c2c8499e7938725fad1cfff8a3fe66ebec8e40
rlaphoenix <pragma.exe@gmail.com>
2022-01-31 16:14:18 +0000

    Add DYNAMIC_RANGE_MAP and AUDIO_CHANNEL_LAYOUT_WEIGHT

--- nfog/constants.py
+++ nfog/constants.py
@@ -2,3 +2,15 @@ GROUP_SETTINGS = dict(
     help_option_names=["-?", "-h", "--help"],
     max_content_width=116  # max PEP8 line-width, -4 to adjust for initial indent
 )
+
+DYNAMIC_RANGE_MAP = {
+    "SMPTE ST 2086": "HDR10",
+    "HDR10": "HDR10",
+    "SMPTE ST 2094 App 4": "HDR10+",
+    "HDR10+": "HDR10+",
+    "Dolby Vision": "DV"
+}
+
+AUDIO_CHANNEL_LAYOUT_WEIGHT = {
+    "LFE": 0.1
+}
