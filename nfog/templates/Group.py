44134cea115c08e71fadd2ad14b50eb3b040c7b5
rlaphoenix <pragma.exe@gmail.com>
2022-01-31 18:19:29 +0000

    Add error handling to TemplateGroup

--- nfog/templates/Group.py
+++ nfog/templates/Group.py
@@ -12,11 +12,12 @@ class TemplateGroup(click.MultiCommand):
     def list_commands(self, ctx: click.Context) -> list[str]:
         """Returns a list of template names from the template filenames."""
         rv = []
-        if not self.TEMPLATES_DIR.is_dir():
-            return rv
-        for cmd in self.TEMPLATES_DIR.iterdir():
-            if cmd.stem.lower() not in ("__init__", "group") and cmd.suffix.lower() == ".py":
-                rv.append(cmd.stem)
+        if self.TEMPLATES_DIR.is_dir():
+            for cmd in self.TEMPLATES_DIR.iterdir():
+                if cmd.stem.lower() not in ("__init__", "group") and cmd.suffix.lower() == ".py":
+                    rv.append(cmd.stem)
+        if not rv:
+            raise click.ClickException(f"No Templates were found in {Directories.templates}")
         rv.sort()
         return rv
 
@@ -24,6 +25,8 @@ class TemplateGroup(click.MultiCommand):
         """Load the template code and return the main click command function."""
         scope: dict[str, Any] = {}
         fn = self.TEMPLATES_DIR / f"{name}.py"
+        if not fn.exists():
+            raise click.ClickException(f"The Template ({name}) was not found in {Directories.templates}.")
         with fn.open(encoding="utf8") as f:
             code = compile(f.read(), fn, "exec")
             eval(code, scope, scope)
