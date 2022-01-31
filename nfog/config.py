13058b411e8a8894541dfa91dfd77e0c89d1f6c4
rlaphoenix <pragma.exe@gmail.com>
2022-01-31 18:11:27 +0000

    Add artwork directory to Directories config

--- nfog/config.py
+++ nfog/config.py
@@ -7,6 +7,7 @@ from appdirs import user_data_dir
 class Directories:
     user_data = Path(user_data_dir("nfog", False))
     templates = user_data / "templates"
+    artwork = user_data / "artwork"
 
 
 class Files:
