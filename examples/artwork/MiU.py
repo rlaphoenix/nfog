cd0b0f8bcf220d54f3d799be99c000f42586bdd1
rlaphoenix <rlaphoenix@pm.me>
2022-07-02 14:38:32 +0100

    -MiU: Add documentation on the Artwork

--- examples/artwork/MiU.py
+++ examples/artwork/MiU.py
@@ -7,6 +7,22 @@ from nfog.templates import Template
 class MiU(Artwork):
     @staticmethod
     def with_template(template: Template) -> str:
+        """
+        The Goal of this Artwork Template is to be a stylish "modern" BBCode NFO.
+
+        It transforms the NFO Template to be a width of 70 characters. It may extend
+        the width beyond 70 characters in some sections if the Release name is longer
+        than 70 characters.
+
+        Without the padding the text would be aligned centered, which is expected. E.g.:
+        |          hey          |
+        |      how are you      |
+
+        But I want to center the "block" of content, not the text:
+        |      hey........      |
+        |      how are you      |
+        (notice the `.` represents the padded spaces, padding to 11 characters)
+        """
         now = datetime.now().strftime("%Y.%m.%d %H:%M")
         nfo = template.nfo.splitlines(keepends=False)
         is_nfo = template.file_ext == ".nfo"
